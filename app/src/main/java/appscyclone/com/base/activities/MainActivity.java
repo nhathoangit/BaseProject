13d2cfe3bcb63302fdc7728417d8985384975394
Nhat Hoang <nhat.hoang@apps-cyclone.com>
2018-04-20 10:40:13 +0700

    + Create package

--- app/src/main/java/appscyclone/com/base/activities/MainActivity.java
+++ app/src/main/java/appscyclone/com/base/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package appscyclone.com.base;
+package appscyclone.com.base.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import appscyclone.com.base.R;
 
 public class MainActivity extends AppCompatActivity {
 
