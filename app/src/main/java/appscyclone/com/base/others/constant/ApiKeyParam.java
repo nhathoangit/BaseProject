409dbe6fb50d6163937ec46f04f6b8fa86b211fa
Nhat Hoang <nhat.hoang@apps-cyclone.com>
2018-06-06 09:50:47 +0700

    + Add ApiKeyParam, AppConstant

---
+++ app/src/main/java/appscyclone/com/base/others/constant/ApiKeyParam.java
@@ -0,0 +1,8 @@
+package appscyclone.com.base.others.constant;
+
+/**
+ * Created by thanh.tran on 5/24/2018.
+ */
+public class ApiKeyParam {
+    public static final String TEXT = "text";
+}
---
+++ app/src/main/java/appscyclone/com/base/others/constant/AppConstant.java
@@ -0,0 +1,8 @@
+package appscyclone.com.base.others.constant;
+
+/**
+ * Created by thanh.tran on 5/24/2018.
+ */
+public class AppConstant {
+    public static final String EMAIL = "email";
+}
